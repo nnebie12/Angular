1ad54d00581beabc548c75935099e792db1755e3
KASSI Diane Rolande <kassinnebie@macbook-pro-de-kassi.home>
2023-07-12 16:20:53 +0200

    implentation du fichier housinglocation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+  HousingLocationComponent,
+
+  ],
   template: `
   <section>
   <form>
@@ -12,9 +18,22 @@ import { CommonModule } from '@angular/common';
     <button class="primary" type="button">Search</button>
   </form>
 </section>
+<section class="results">
+    <app-housing-location></app-housing-location>
+</section>
   `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: 'assets/example-house.jpg',
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 
 }
